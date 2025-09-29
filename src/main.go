6297a599bb1ef078c23905f59e1d4cf8c95f6085
Bryan <bryan.l.bergo@gmail.com>
2025-09-29 09:29:01 -0700

    feat: healthcheck

--- src/main.go
+++ src/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Starting up Barbican ES...")
+	http.HandleFunc(("/health"), func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	println("Barbican ES proxy listening on :3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
